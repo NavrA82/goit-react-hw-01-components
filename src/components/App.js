c07cd94f6362211205f9372709a30337414d7240
NavrA82 <124286244+NavrA82@users.noreply.github.com>
2023-08-07 00:10:24 +0300

    1-3

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
-import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+// import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 import profileCard from '../data/user.json';
 import stats from '../data/data.json';
 import friends from '../data/friends.json';
-import transactions from '../data/transactions.json';
+// import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
@@ -14,7 +14,7 @@ export const App = () => {
       <Profile card={profileCard} />
       <Statistics stats={stats} />
       <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
+      {/* <TransactionHistory items={transactions} /> */}
     </div>
   );
 };
