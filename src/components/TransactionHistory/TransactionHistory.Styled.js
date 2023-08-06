5bdbaaa08865ded563ce0021abe3515a93e1cc59
NavrA82 <124286244+NavrA82@users.noreply.github.com>
2023-08-07 00:33:48 +0300

    4.3

--- src/components/TransactionHistory/TransactionHistory.Styled.js
+++ src/components/TransactionHistory/TransactionHistory.Styled.js
@@ -6,6 +6,8 @@ export const Table = styled.table`
   width: 360px;
   border-radius: 8px;
   margin-bottom: 40px;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px,
+    rgba(0, 0, 0, 0.22) 0px 10px 10px;
 `;
 
 export const Thead = styled.thead`
